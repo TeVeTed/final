b0808c0fbbcd274fce72ff05f12428fc4447e1c6
TeVeTed <35794339+TeVeTed@users.noreply.github.com>
2018-02-01 08:52:32 +0200

    some styles and a little bit more

--- js/controllers/statisticController.js
+++ js/controllers/statisticController.js
@@ -1,9 +1,20 @@
 app.controller('statisticController',
     function statisticController($scope, $localStorage) {
+        $scope.storedStatistic = angular.fromJson($localStorage.statistic);
+
         $scope.getHeaders = function () {
-            $scope.storedStatistic = angular.fromJson($localStorage.statistic);
             var data = angular.fromJson($scope.storedStatistic[0]);
             return Object.keys(data);
         }
-        console.log($scope.getHeaders());
+
+
+        $scope.stats = [];
+        $scope.getStats = function () {
+            for (i=0; i<=$scope.storedStatistic.length-1; i++) {
+                $scope.stats.push(angular.fromJson($scope.storedStatistic[i]));
+            }
+        }
+        $scope.getStats();
+
+        console.log($scope.stats);
 });
